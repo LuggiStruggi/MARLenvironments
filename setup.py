d14bbc3c66626b6a0ae66723968b587840bf6030
LuggiStruggi <lukasmkoenig@gmx.net>
2022-02-02 13:29:55 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='marlenvs', version='0.1', python_requires='>=3.9', install_requires=['gym', 'numpy', 'wheel', 'pyglet'])
+setup(
+		name='marlenvs',
+		version='1.0.2',
+		author='Lukas König',
+		author_email='lukasmkoenig@gmx.net',
+        packages=find_packages(),
+		python_requires='>=3.9',
+		install_requires=['gym', 'numpy', 'wheel', 'pyglet'],
+		decription="Multi-Agent Reinforcement Learning environments for gym."
+	)
