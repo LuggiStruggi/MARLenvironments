d09fb7032cde7912ef60cfa0c8751896accdc94d
LuggiStruggi <lukasmkoenig@gmx.net>
2022-02-24 11:17:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
 		author='Lukas König',
 		author_email='lukasmkoenig@gmx.net',
         packages=find_packages(),
-		python_requires='>=3.9',
 		install_requires=['gym', 'numpy', 'wheel', 'pyglet'],
 		decription="Multi-Agent Reinforcement Learning environments for gym."
 	)
